61684243a04832e2c4cc9b9ffc32c20de8c85131
Julio Arias <jarias01@gmail.com>
2015-08-21 11:41:05 -0600

    Check if ansible is installed

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#Install Ansible
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install ansible
+if [ ! -x /usr/bin/ansible ]; then
+	#Install Ansible
+	sudo apt-get install software-properties-common
+	sudo apt-add-repository ppa:ansible/ansible
+	sudo apt-get update
+	sudo apt-get install ansible
+fi
 
 #Run Ansible
 PYTHONIOENCODING='utf-8' ansible-playbook desktop.yml --ask-sudo-pass
