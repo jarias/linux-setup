498c0850498c451266fc5c1d287287fd1910cb3a
Julio Arias Carrillo <jarias01@gmail.com>
2016-12-15 12:13:39 -0600

    Made dotfiles repo and backups configurable

--- linux-setup.sh
+++ linux-setup.sh
@@ -12,5 +12,14 @@ if [ -z "$ARTIFACTORY_USERNAME" ] || [ -z $ARTIFACTORY_PASSWORD ]; then
   exit 1
 fi
 
+if [ -z "$DOTFILES_REPO" ]; then
+  echo "Missing dotfiles Github repo URL"
+  exit 1
+fi
+
+CONFIGURE_BACKUPS="${CONFIGURE_BACKUPS:-yes}"
+
 #Run Ansible
-ansible-playbook desktop.yml --ask-become-pass --extra-vars "artifactory_username=$ARTIFACTORY_USERNAME artifactory_password=$ARTIFACTORY_PASSWORD"
+ansible-playbook desktop.yml \
+  --ask-become-pass \
+  --extra-vars "dotfiles_repo=${DOTFILES_REPO} configure_backups=${CONFIGURE_BACKUPS} artifactory_username=${ARTIFACTORY_USERNAME} artifactory_password=${ARTIFACTORY_PASSWORD}"
